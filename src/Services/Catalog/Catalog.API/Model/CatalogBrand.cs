e2a38134106149429df418f9ae76e99f52934e37
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2021-08-06 18:35:51 +0530

    catlog-api: Model added.

---
+++ src/Services/Catalog/Catalog.API/Model/CatalogBrand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Services.Catalog.API.Model
+{
+    public class CatalogBrand
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Model/CatalogType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Services.Catalog.API.Model
+{
+    public class CatalogType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
